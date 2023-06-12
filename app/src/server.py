c5ca7932a6a36bf6a01fb2573f1e00e31723467a
whtsht <whiteshirt0079@gmail.com>
2023-06-12 22:57:17 +0900

    APPサーバの開発環境構築

---
+++ app/src/server.py
@@ -0,0 +1,30 @@
+from flask import Flask
+from enum import Enum
+from web import web
+import logging
+
+
+class Mode(Enum):
+    Prod = (1,)
+    Dev = (2,)
+
+
+def create_app(mode: Mode):
+    app = Flask(__name__)
+    if mode == Mode.Dev:
+        logging.basicConfig(level=logging.INFO)
+        app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///test.db"
+    else:
+        logging.basicConfig(level=logging.ERROR)
+        app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///data.db"
+
+    app.register_blueprint(web)
+    return app
+
+
+def create_prod_app():
+    return create_app(Mode.Prod)
+
+
+def create_dev_app():
+    return create_app(Mode.Dev)

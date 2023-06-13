5cf30d26334a0d883ed1e3b430f9b30990942583
al21016 <zhiyaojude@gmail.com>
2023-06-13 16:28:26 +0900

    web: M2 予定追加要求送信を実装

---
+++ web/src/components/addPlan.tsx
@@ -0,0 +1,35 @@
+/**
+ * Designer    : 菊地智遥
+ * Date        : 2023/6/13
+ * Purpose     : Appサーバーに対して，HTTPリクエストを送信する．
+ */
+
+import { Plan } from "../plan";
+
+/**
+ * Appサーバーに対して，HTTPリクエストを送信する．
+ *
+ * @param lineID   - ユーザーのLineID
+ * @param plan     - 予定の情報
+ * @returns
+ */
+
+async function addPlan(lineID: string, plan: Plan): Promise<boolean> {
+  try {
+    await fetch("/web/add_plan/", {
+      method: "POST",
+      headers: {
+        "content-type": "application/json;charset=UTF-8",
+      },
+      body: JSON.stringify({
+        lineID: lineID,
+        plan: plan,
+      }),
+    });
+    return true;
+  } catch (e) {
+    return false;
+  }
+}
+
+export { addPlan };

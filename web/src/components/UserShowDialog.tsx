f67c28348ab3466402d05653f70c85ad2e7e8109
al21016 <zhiyaojude@gmail.com>
2023-06-13 16:52:45 +0900

    web: M12 pushの確認

---
+++ web/src/components/UserShowDialog.tsx
@@ -0,0 +1,18 @@
+/**
+ * Designer    : 菊地智遥
+ * Date        : 2023/6/
+ * Purpose     :
+ */
+
+/**
+ * Appサーバーに対して，HTTPリクエストを送信する．
+ *
+ * @param lineID   - ユーザーのLineID
+ * @returns
+ */
+
+function UserShowDialog() {
+  return <h1>hello</h1>;
+}
+
+export default UserShowDialog;

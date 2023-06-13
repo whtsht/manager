52e0e502c56fe284219a7828511d33011fb97ff4
whtsht <whiteshirt0079@gmail.com>
2023-06-13 21:33:54 +0900

    web: M1 予定リスト取得要求送信を実装

---
+++ web/src/components/getPlanList.tsx
@@ -0,0 +1,33 @@
+/**
+ * Designer    : 東間日向
+ * Date        : 2023/6/13
+ * Purpose     : Appサーバーから指定利用者の予定情報のリストを取得する
+ */
+
+import { Plan } from "../plan";
+
+/**
+ * Appサーバーに対してGETリクエストを送信し，予定情報のリストを取得する．
+ *
+ * @param lineID - ユーザーのLineID
+ * @returns 予定情報のリスト
+ */
+async function getPlanList(lineID: string): Promise<[Plan] | null> {
+    try {
+        const response = await fetch("/web/get_plan_list/", {
+            method: "GET",
+            headers: {
+                "content-type": "application/json;charset=UTF-8",
+            },
+            body: JSON.stringify({
+                lineID: lineID,
+            }),
+        });
+
+        return response.json();
+    } catch (_) {
+        return null;
+    }
+}
+
+export { getPlanList };

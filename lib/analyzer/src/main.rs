0588e8c42ae39882feefb1abba72941c2c8b4316
whtsht <whiteshirt0079@gmail.com>
2023-06-16 17:19:15 +0900

    app: M15 操作名，予定名取得処理を実装

--- lib/analyzer/src/main.rs
+++ lib/analyzer/src/main.rs
@@ -2,7 +2,24 @@ pub mod basic;
 pub mod data_time;
 pub mod date;
 pub mod digits;
+pub mod name_op;
 pub mod time;
+
+use data_time::DateTime;
+use serde::Serialize;
+
+#[derive(Debug, Serialize, PartialEq, Eq)]
+pub enum Operation {
+    Add,
+    Search,
+}
+
+#[derive(Debug, Serialize, PartialEq, Eq)]
+pub struct Response {
+    pub title: Option<String>,
+    pub operation: Option<Operation>,
+    pub date_time: DateTime,
+}
 
 fn main() {
     println!("Hello, world!");

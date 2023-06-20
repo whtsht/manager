919b7f55bd4f2bfad037d47a0041968b4f392ee9
whtsht <whiteshirt0079@gmail.com>
2023-06-20 10:15:22 +0900

    app: コメントの修正

--- app/src/plan/main.py
+++ app/src/plan/main.py
@@ -3,14 +3,13 @@ from info import UserState
 
 states: dict[str, UserState] = {}
 
-"""利用者の現在の状態と入力を統合する
-Args: line_id:LineID
-      input_info:入力情報
-Returns: Input_info:入力情報
-"""
-
 
 def integrate_input(line_id: str, input_info: InputInfo) -> InputInfo:
+    """利用者の現在の状態と入力を統合する
+    Args: line_id:LineID
+          input_info:入力情報
+    Returns: Input_info:入力情報
+    """
     if line_id in states:
         input_info.title = input_info.title or states[line_id].plan_info.title
         input_info.op = input_info.op or states[line_id].op

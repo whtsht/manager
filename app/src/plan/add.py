71abbd45c293dba5c5c1cf02709c1469a8e3dbbd
al21016 <zhiyaojude@gmail.com>
2023-06-20 16:57:26 +0900

    app:M26,M27,M28を更新

--- app/src/plan/add.py
+++ app/src/plan/add.py
@@ -1,3 +1,9 @@
+"""
+Dedigner: 菊地智遥
+Date: 2023/6/20
+Purpose: 
+"""
+
 from db_models import db, Plan
 from info import PlanInfo, AddError
 
@@ -7,11 +13,18 @@ def from_message(lineID: str, plan_info: PlanInfo):
 
 
 def add_plan(plan: Plan):
+    """DBに予定を追加する
+    Args: plan: 予定の情報
+    Returns: なし
+    """
     db.session.add(plan)
     db.session.comit()
 
 
 def uncompleted_message(error: AddError):
+    """エラーメッセージを作成する
+    Args: error: どのタイプのエラーか
+    Returns: エラーメッセージ"""
     if error.error_type == 1:
         return "その予定は既に追加されています"
     if error.error_type == 2:
@@ -21,4 +34,14 @@ def uncompleted_message(error: AddError):
 
 
 def complited_message(plan_info: PlanInfo):
-    return f"予定の追加が完了しました。タイトルは{plan_info.title}、開始時間は{plan_info.start_time}です。"
+    """完了メッセージを作成する
+
+    Args:
+        plan_info (PlanInfo): 予定情報
+
+    Returns:
+        mes (str): 完了メッセージ
+    """
+    mes = f"予定の追加が完了しました。タイトルは{plan_info.title}、開始時間は{plan_info.start_time}です。"
+
+    return mes

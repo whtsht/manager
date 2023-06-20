0752e37d22d63bd6cea007447349a245748a0e24
whtsht <whiteshirt0079@gmail.com>
2023-06-20 10:43:25 +0900

    app: M36 予定修正処理 引数を修正

--- app/src/plan/modify.py
+++ app/src/plan/modify.py
@@ -1,3 +1,9 @@
+"""
+Designer:   東間日向
+Date:       2023/06/20
+Purpose:    予定修正に関する関数
+"""
+
 from web import web
 from flask import request
 from db_models import Plan, db
@@ -5,10 +11,12 @@ from db_models import Plan, db
 
 @web.route("/modify_plan/", methods=["POST"])
 def modify_plan():
+    """M36 予定修正処理
+    M3の要求を受け，予定を修正する．データベースからPlanのidと一致する予定を見つけ，それを更新する．
+    """
     if data := request.json:
-        plan_id: str = data["planID"]
         plan: Plan = data["plan"]
-        db_plan: Plan = Plan.query.filter_by(Plan.id == plan_id).first()
+        db_plan: Plan = Plan.query.filter_by(Plan.id == plan.id).first()
         db_plan.title = plan.title
         db_plan.detail = plan.detail
         db_plan.notif_time = plan.notif_time

40d212f70b4b058ac23c4af7f5a0254fab1063e7
al21073 <al21073@shibaura-it.ac.jp>
2023-06-20 13:54:49 +0900

    web:M13lineログインボタン（仮）

---
+++ web/src/components/line_login_bottun.tsx
@@ -0,0 +1,44 @@
+/**
+ * Designer    : 小田桐光佑
+ * Date        : 2023/6/
+ * Purpose     : lineログインのためのボタン（デザインは仮）
+ */
+
+import React from "react";
+
+interface Props {
+  border: string;
+  color: string;
+  children?: React.ReactNode;
+  height: string;
+  onClick: () => void;
+  radius: string
+  width: string;
+}
+
+const Button: React.FC<Props> = ({ 
+    border,
+    color,
+    children,
+    height,
+    onClick, 
+    radius,
+    width
+  }) => { 
+  return (
+    <button 
+      onClick={onClick}
+      style={{
+         backgroundColor: color,
+         border,
+         borderRadius: radius,
+         height,
+         width
+      }}
+    >
+    {children}
+    </button>
+  );
+}
+
+export default Button;

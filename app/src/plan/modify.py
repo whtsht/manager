c76cfcb71f150814c16c5febf8b12ec15732b9eb
whtsht <whiteshirt0079@gmail.com>
2023-06-27 19:17:10 +0900

    app: 不要なファイルを削除

--- app/src/plan/modify.py
+++ app/src/plan/modify.py
@@ -1,28 +1,24 @@
 """
-Designer:   東間日向
-Date:       2023/06/20
+Designer:   石川隼
+Date:       2023/06/24
 Purpose:    予定修正に関する関数
 """
 
-from web import web
-from flask import request
-from db_models import Plan, db
+from info import Plan, db
 
 
-@web.route("/modify_plan/", methods=["POST"])
-def modify_plan():
-    """M36 予定修正処理
-    M3の要求を受け，予定を修正する．データベースからPlanのidと一致する予定を見つけ，それを更新する．
+def modify_plan(_: int, plan: Plan):
+    """M30 予定修正処理
+
+    Args:
+        plan_id (int): 予定情報
+        plan (Plan): 予定
     """
-    if data := request.json:
-        plan: Plan = data["plan"]
-        db_plan: Plan = Plan.query.filter_by(Plan.id == plan.id).first()
-        db_plan.title = plan.title
-        db_plan.detail = plan.detail
-        db_plan.notif_time = plan.notif_time
-        db_plan.allday = plan.allday
-        db_plan.start_time = plan.start_time
-        db_plan.end_time = plan.end_time
-        db.session.commit()
-
-    return "ok"
+    db_PLAN: Plan = Plan.query.filter_by(Plan.id == plan.id).first()
+    db_PLAN.title = plan.title
+    db_PLAN.detail = plan.detail
+    db_PLAN.notif_time = plan.notif_time
+    db_PLAN.allday = plan.allday
+    db_PLAN.start_time = plan.start_time
+    db_PLAN.end_time = plan.end_time
+    db.session.commit()

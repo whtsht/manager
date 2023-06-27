79cc452c9802f01217b6326ac02e57499618c191
al21073 <al21073@shibaura-it.ac.jp>
2023-06-27 17:22:56 +0900

    app:M19 コード完成

---
+++ app/src/plan/gen_message.py
@@ -0,0 +1,41 @@
+"""
+Dedigner: 小田桐光佑
+Date: 2023/6/27
+Purpose:M19 現在の利用者の状態から返答メッセージを生成する.
+"""
+from plan import add, search
+from plan.main import status
+from info import OP
+
+
+def gen_message(lineID: str):
+    """.statusからop/complitedを取り出す.
+    もしAdd/TrueならばM28を呼び出す.
+    もしAdd/FalseならばM27を呼び出す.
+    もしSearch/TrueならばM33を呼び出す.
+    もしSearch/FalseならばM32を呼び出す.
+
+    Args:
+        lineID (str): lineID
+        PlanInfo (plan_info): 予定情報
+        AddError (error): 予定情報エラー
+        list (Plan): 予定リスト
+        SerchError (error): 検索エラー
+    """
+    st = status[lineID]
+    message = ""
+    if st.op == OP.Add and st.complited is True:
+        message = add.complited_message(st.plan_info)
+        del status[lineID]
+
+    elif st.op == OP.Add and st.complited is False:
+        message = add.uncompleted_message(st.add_error)
+
+    elif st.op == OP.Search and st.complited is True:
+        message = search.completed_message(st.plan_info, st.plan_list)
+        del status[lineID]
+
+    elif st.op == OP.Search and st.complited is False:
+        message = search.uncompleted_message(st.search_error)
+
+    return message

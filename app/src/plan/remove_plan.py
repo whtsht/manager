9d7190063703f71134c604914a16787ce82908e7
ishikawa <al21094@shibaura-it.ac.jp>
2023-06-27 16:46:56 +0900

    app: M29 予定削除処理の修正

--- app/src/plan/remove_plan.py
+++ app/src/plan/remove_plan.py
@@ -4,14 +4,16 @@ Date:       2023/06/24
 Purpose:    予定の削除処理
 """
 
-from info import PlanInfo
+from info import PlanInfo,Plan, db
 from web import web
 from flask import request
-from db_models import Plan, db
+
 
 
 def remove_plan(plan_id: str):
-    """M29 予定削除処理
+    """M29 予定削除処理　予定をデータベースから削除
+    Args: 
+        str (plan_id): 予定のid
     """
     plan = db.session.query(Plan).filter(Plan.plan_id == plan_id).first()
     db.session.delete(plan)

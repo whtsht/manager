0a864a7dfc3542e61cfb2f615407ebbae880909d
whtsht <whiteshirt0079@gmail.com>
2023-06-27 16:26:50 +0900

    app: インポートの修正

--- app/src/plan/remove_plan.py
+++ app/src/plan/remove_plan.py
@@ -4,20 +4,11 @@ Date:       2023/06/24
 Purpose:    予定の削除処理
 """
 
-from info import PlanInfo
-from web import web
-from flask import request
-from db_models import Plan, db
+from info import Plan, db
 
 
 def remove_plan(plan_id: str):
-    """M29 予定削除処理
-    """
+    """M29 予定削除処理"""
     plan = db.session.query(Plan).filter(Plan.plan_id == plan_id).first()
     db.session.delete(plan)
     db.session.commit()
-
-
-
-
-

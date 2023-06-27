dc3523b8eefb369c5ea117357e71ac86982bb1bf
ishikawa <al21094@shibaura-it.ac.jp>
2023-06-27 17:10:51 +0900

    app:M30 予定修正処理の修正

--- app/src/plan/modify_plan.py
+++ app/src/plan/modify_plan.py
@@ -6,22 +6,23 @@ Purpose:    予定修正に関する関数
 
 from web import web
 from flask import request
-from db_models import Plan, db
+from info import Plan, db
 
 
-@web.route("/modify_plan/", methods=["POST"])
-def modify_plan():
+def modify_plan(plan_id:int, plan:Plan):
     """M30 予定修正処理
+
+    Args:
+        plan_id (int): 予定情報
+        plan (Plan): 予定
     """
     if data := request.json:
-        plan: Plan = data["plan"]
-        db_plan: Plan = Plan.query.filter_by(Plan.id == plan.id).first()
-        db_plan.title = plan.title
-        db_plan.detail = plan.detail
-        db_plan.notif_time = plan.notif_time
-        db_plan.allday = plan.allday
-        db_plan.start_time = plan.start_time
-        db_plan.end_time = plan.end_time
+        PLAN: Plan = data["plan"]
+        db_PLAN: Plan = Plan.query.filter_by(Plan.id == plan.id).first()
+        db_PLAN.title = plan.title
+        db_PLAN.detail = plan.detail
+        db_PLAN.notif_time = plan.notif_time
+        db_PLAN.allday = plan.allday
+        db_PLAN.start_time = plan.start_time
+        db_PLAN.end_time = plan.end_time
         db.session.commit()
-
-    return "ok"

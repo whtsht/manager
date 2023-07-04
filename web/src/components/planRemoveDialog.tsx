47d5674d91a22681bbe271feef80f036d39c335a
ishikawa <al21094@shibaura-it.ac.jp>
2023-07-04 17:43:08 +0900

    web M11 予定削除処理を実装

---
+++ web/src/components/planRemoveDialog.tsx
@@ -0,0 +1,59 @@
+/**
+ * Designer    : 石川隼
+ * Date        : 2023/6/15
+ * Purpose     :
+ */
+import { Plan } from "../plan";
+/**
+ *
+ *
+ * @param open          - 表示フラグ
+ * @param handleClose   - 閉じ関数
+ * @param plan          - 予定の情報
+ * @param removePlan    - 予定の削除
+ * @returns dialog      - ダイアログ表示のコンポーネント
+ */
+
+import Dialog from "@mui/material/Dialog";
+import Button from "@mui/material/Button";
+import DialogActions from "@mui/material/DialogActions";
+import DialogTitle from "@mui/material/DialogTitle";
+
+function PlanRemoveDialog({
+  open,
+  handleClose,
+  plan,
+  removePlan,
+}: {
+  open: boolean;
+  handleClose: () => void;
+  plan: Plan;
+  removePlan: (id: number) => Promise<void>;
+}) {
+  return (
+    <Dialog
+      open={open}
+      onClose={handleClose}
+      aria-labelledby="alert-dialog-title"
+    >
+      <DialogTitle id="alert-dialog-title">
+        {"予定を削除しますか？"}
+      </DialogTitle>
+      <DialogActions>
+        <Button
+          onClick={async () => {
+            await removePlan(plan.id);
+            handleClose();
+          }}
+        >
+          はい
+        </Button>
+        <Button onClick={handleClose} autoFocus>
+          いいえ
+        </Button>
+      </DialogActions>
+    </Dialog>
+  );
+}
+
+export default PlanRemoveDialog;

a24c2f972fa813078b988373c306464ceffd22e3
whtsht <whiteshirt0079@gmail.com>
2023-07-04 21:37:32 +0900

    web: 予定修正処理の引数変更

--- web/src/components/modifyPlan.tsx
+++ web/src/components/modifyPlan.tsx
@@ -7,26 +7,24 @@ import { Plan } from "../plan";
 /**
  * Appサーバーに対して，HTTPリクエストを送信する．
  *
- * @param lineID   - ユーザーのLineID
  * @param plan     - 予定の情報
  * @returns
  */
-async function modifyPlan(lineID: string, plan: Plan): Promise<boolean> {
-  try {
-    await fetch("/web/modifyPlan/", {
-      method: "POST",
-      headers: {
-        "content-type": "application/json;charset=UTF-8",
-      },
-      body: JSON.stringify({
-        lineID: lineID,
-        plan: plan,
-      }),
-    });
-    return true;
-  } catch (e) {
-    return false;
-  }
+async function modifyPlan(plan: Plan): Promise<boolean> {
+    try {
+        await fetch("/web/modifyPlan/", {
+            method: "POST",
+            headers: {
+                "content-type": "application/json;charset=UTF-8",
+            },
+            body: JSON.stringify({
+                plan: plan,
+            }),
+        });
+        return true;
+    } catch (e) {
+        return false;
+    }
 }
 
 export { modifyPlan };
